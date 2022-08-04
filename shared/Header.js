779697f506948243ca09a6360d896a1446a6f53d
hasibulislam999 <hasib143sl@gmail.com>
2022-08-04 23:35:15 +0600

    header modification done

--- shared/Header.js
+++ shared/Header.js
@@ -13,34 +13,34 @@ const Header = () => {
     </>
   );
   return (
-    <section className="container mx-auto">
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex="0" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <section className="bg-base-200">
+      <div className="container mx-auto">
+        <div className="navbar bg-base-200">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex="0"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded w-52"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex="0"
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded w-52"
-            >
-              <div className="flex flex-col gap-x-4">{navMenu}</div>
-            </ul>
-          </div>
-          <div className="lg:mr-auto ml-auto">
+                <div className="flex flex-col gap-x-4">{navMenu}</div>
+              </ul>
+            </div>
             <Link href="/">
               <Image
                 alt="luxury living logo"
@@ -50,14 +50,16 @@ const Header = () => {
               />
             </Link>
           </div>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">
-            <div className="flex flex-row gap-x-4">{navMenu}</div>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn btn-sm rounded px-6 capitalize">Login</a>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              <div className="flex flex-row gap-x-4">{navMenu}</div>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <a className="btn btn-sm rounded px-6 capitalize bg-[#251D58]">
+              Login
+            </a>
+          </div>
         </div>
       </div>
     </section>
