d03c4f889566155ca6d40acad9b7ba2a2b5250e3
hasibulislam999 <hasib143sl@gmail.com>
2022-08-05 21:03:45 +0600

    section header modified

--- components/SectionHeader.js
+++ components/SectionHeader.js
@@ -1,8 +1,8 @@
 const SectionHeader = ({ name, title }) => {
   return (
-    <section className="text-center">
+    <section className="text-center my-8">
       <h6 className="font-bold">{name}</h6>
-      <h2 className="text-3xl font-bold w-1/3 mx-auto">{title}</h2>
+      <h2 className="text-3xl font-bold lg:w-1/3 md:w-1/2 mx-auto">{title}</h2>
     </section>
   );
 };
