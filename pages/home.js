5399ae45d968545d6263263e62a01ac7c7bd7191
hasibulislam999 <hasib143sl@gmail.com>
2022-08-05 21:04:32 +0600

    home modified with projects

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import Projects from "../components/Projects";
 
 const Home = () => {
   return (
     <section>
       <Banner />
+      <Projects />
     </section>
   );
 };
