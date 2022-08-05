5dab62eef63b5aa4fb0dbc9be0eb022a03770ca2
hasibulislam999 <hasib143sl@gmail.com>
2022-08-05 12:49:35 +0600

    projects edited

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,13 @@
+import SectionHeader from "../components/SectionHeader";
+
 const Projects = () => {
-  return <section>Projects route</section>;
+  return (
+    <section>
+      <SectionHeader
+        name={"Projects"}
+        title={"Discover the latest Interior Design available today"}
+      />
+    </section>
+  );
 };
 export default Projects;
