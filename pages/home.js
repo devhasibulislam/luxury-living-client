4122cdb85840b87ed5f463950ba6f04b95a64fc2
hasibulislam999 <hasib143sl@gmail.com>
2022-08-06 13:33:03 +0600

    home modification done

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,13 @@
 import Banner from "../components/Banner";
 import Projects from "../components/Projects";
+import Services from "../components/Services";
 
 const Home = () => {
   return (
     <section>
       <Banner />
       <Projects />
+      <Services />
     </section>
   );
 };
