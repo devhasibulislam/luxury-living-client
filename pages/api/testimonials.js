881a4422c598513ece5fc3b45a9e9ae497d0c4f0
hasibulislam999 <hasib143sl@gmail.com>
2022-08-07 20:14:01 +0600

    testimonials api bug fix

--- pages/api/testimonials.js
+++ pages/api/testimonials.js
@@ -1,4 +1,4 @@
-import testimonials from "./testimonials";
+import testimonials from "../../JSON/testimonials.json";
 
 export default function handler(req, res) {
   res.status(200).json(testimonials);
