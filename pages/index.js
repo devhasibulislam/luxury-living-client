3566891c04ecb94749f081e0b5af0e7e2e82c05a
hasibulislam999 <hasib143sl@gmail.com>
2022-08-08 00:48:20 +0600

    index updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
-import Home from "./home";
+import Banner from "../components/Banner";
+import Contact from "../components/Contact";
+import Projects from "../components/Projects";
+import Services from "../components/Services";
+import Testimonials from "../components/Testimonials";
 
 const Index = () => {
-  return <Home />;
+  return (
+    <section>
+      <Banner />
+      <Projects />
+      <Services />
+      <Testimonials />
+      <Contact />
+    </section>
+  );
 };
 export default Index;
