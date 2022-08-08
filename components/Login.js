3ca09892854171963b68f363ed3238fd1faa1e05
hasibulislam999 <hasib143sl@gmail.com>
2022-08-08 10:52:12 +0600

    google authentication integrated

--- components/Login.js
+++ components/Login.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import auth from "../config/firebase";
 import Modal from "./Modal";
 import Register from "./Register";
 import Reset from "./Reset";
 import toast from "react-hot-toast";
+import Image from "next/image";
 
 const Login = ({ showModalLogin, setShowModalLogin }) => {
   const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
   const [showModalReset, setShowModalReset] = useState(false);
   const [showModalRegistration, setShowModalRegistration] = useState(false);
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
   const register = "Sign up";
   const reset = "Reset password";
 
@@ -69,6 +74,19 @@ const Login = ({ showModalLogin, setShowModalLogin }) => {
                 <button className="btn btn-sm rounded px-6 capitalize bg-[#251D58]">
                   Sign In
                 </button>
+                <div
+                  className="flex items-center justify-between border rounded-full w-full mt-8"
+                  onClick={() => signInWithGoogle()}
+                >
+                  <Image
+                    src="/assets/google.svg"
+                    alt="google sign in logo"
+                    height={31}
+                    width={31}
+                  />
+                  <span className="font-semibold">Sign in with Google</span>
+                  <span></span>
+                </div>
               </form>
             </div>
           </div>
