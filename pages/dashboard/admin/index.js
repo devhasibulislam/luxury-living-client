00a982bd0ec5b57716885d2ccfe3c7e36f19fa94
hasibulislam999 <hasib143sl@gmail.com>
2022-08-11 14:04:15 +0600

    index route modified

--- pages/dashboard/admin/index.js
+++ pages/dashboard/admin/index.js
@@ -1,4 +1,19 @@
+import Head from "next/head";
+import Layout from "../../../components/Layout";
+import OrderList from "./orderList";
+
 const Admin = () => {
-  return <section>Admin route</section>;
+  return (
+    <section>
+      <Head>
+        <title>Admin - Luxury Living</title>
+      </Head>
+      <section className="">
+        <Layout>
+          <OrderList />
+        </Layout>
+      </section>
+    </section>
+  );
 };
 export default Admin;
