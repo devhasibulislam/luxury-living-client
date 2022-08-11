61125bf0e5438d053e5798561ba17ba01496cb6c
hasibulislam999 <hasib143sl@gmail.com>
2022-08-11 14:04:57 +0600

    booking list route modified

---
+++ pages/dashboard/customer/bookingList.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Layout from "../../../components/Layout";
+const BookingList = () => {
+  return <section><Layout>This is booking list page.</Layout></section>;
+};
+export default BookingList;
