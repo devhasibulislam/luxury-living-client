e3132ededd649aef7d2a8f2f8ab5a2ecbf507162
hasibulislam999 <hasib143sl@gmail.com>
2022-08-12 01:20:42 +0600

    booking list updated

--- pages/dashboard/customer/bookingList.js
+++ pages/dashboard/customer/bookingList.js
@@ -1,6 +1,69 @@
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import Layout from "../../../components/Layout";
+import Mockup from "../../../components/Mockup";
+import Head from "next/head";
+
 const BookingList = () => {
-  return <section><Layout>This is booking list page.</Layout></section>;
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    const getServces = async () => {
+      const request = await fetch("/api/services");
+      const response = await request.json();
+      setServices(response);
+    };
+    getServces();
+  }, []);
+
+  return (
+    <section>
+      <Head>
+        <title>Booking List - Luxury Living</title>
+      </Head>
+      <Layout>
+        <section className="w-3/4">
+          <Mockup>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+              {services.map((service) => (
+                <div
+                  key={service._id}
+                  className="bg-white p-4 rounded-lg shadow flex flex-col gap-y-2"
+                >
+                  <div className="grid grid-cols-2">
+                    <span>
+                      <Image
+                        src={service.thumbnail}
+                        alt={service.title}
+                        height={60}
+                        width={60}
+                      />
+                    </span>
+                    <span className="text-right">
+                      <span
+                        className={`px-4 py-2 rounded ${
+                          (service.status === "Done" &&
+                            "bg-green-300 text-green-800") ||
+                          (service.status === "Pending" &&
+                            "bg-red-300 text-red-800") ||
+                          (service.status === "Ongoing" &&
+                            "bg-orange-300 text-orange-700")
+                        }`}
+                      >
+                        {service.status}
+                      </span>
+                    </span>
+                  </div>
+                  <h3 className="font-bold text-xl">{service.title}</h3>
+                  <p className="text-justify text-gray-500">
+                    {service.description}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </Mockup>
+        </section>
+      </Layout>
+    </section>
+  );
 };
 export default BookingList;
