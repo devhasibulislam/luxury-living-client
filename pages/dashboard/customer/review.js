bea6bbc7824379182d0db0b75033a8f1ee77caba
hasibulislam999 <hasib143sl@gmail.com>
2022-08-12 01:20:50 +0600

    review integration done

--- pages/dashboard/customer/review.js
+++ pages/dashboard/customer/review.js
@@ -1,6 +1,55 @@
+import Head from "next/head";
 import React from "react";
 import Layout from "../../../components/Layout";
+import Mockup from "../../../components/Mockup";
+import styles from "../../../styles/customer/book.module.css";
+
 const Review = () => {
-  return <section><Layout>This is review page.</Layout></section>;
+  return (
+    <section>
+      <Head>
+        <title>Review - Luxury Living</title>
+      </Head>
+      <Layout>
+        <section className="lg:w-1/2 md:w-3/4 w-full">
+          <Mockup>
+            <form>
+              <div className="grid grid-cols-1 gap-y-4 lg:min-w-[500px] md:min-w-[450px]">
+                <div>
+                  <input
+                    className={styles.inputter}
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Your Name"
+                  />
+                </div>
+                <div>
+                  <input
+                    className={styles.inputter}
+                    type="text"
+                    name="designation"
+                    id="designation"
+                    placeholder="Your Designation"
+                  />
+                </div>
+                <div>
+                  <textarea
+                    name="description"
+                    id="description"
+                    className="w-full h-40 p-4 rounded-lg shadow"
+                    placeholder="Write Comment"
+                  />
+                </div>
+              </div>
+              <button className="btn btn-md rounded bg-[#251D58] text-white w-fit px-8 capitalize mt-4">
+                Submit
+              </button>
+            </form>
+          </Mockup>
+        </section>
+      </Layout>
+    </section>
+  );
 };
 export default Review;
