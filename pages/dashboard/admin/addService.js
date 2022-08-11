5c19d46d7415ebc3512c7b92df78c7a2d57280be
hasibulislam999 <hasib143sl@gmail.com>
2022-08-11 14:04:03 +0600

    add service route modified

---
+++ pages/dashboard/admin/addService.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Layout from "../../../components/Layout";
+const AddService = () => {
+  return <section><Layout>This is add service page.</Layout></section>;
+};
+export default AddService;
