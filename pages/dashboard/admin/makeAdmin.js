76468e264c3cb343fff6838b83107ab3ce65c906
hasibulislam999 <hasib143sl@gmail.com>
2022-08-11 14:04:22 +0600

    make admin route modified

---
+++ pages/dashboard/admin/makeAdmin.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Layout from "../../../components/Layout";
+const MakeAdmin = () => {
+  return <section><Layout>This is make admin page.</Layout></section>;
+};
+export default MakeAdmin;
