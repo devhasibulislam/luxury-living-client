1fd4a84a77b66e65a3aed1cde27681c02b571a2f
hasibulislam999 <hasib143sl@gmail.com>
2022-08-11 14:05:10 +0600

    review route modified

---
+++ pages/dashboard/customer/review.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Layout from "../../../components/Layout";
+const Review = () => {
+  return <section><Layout>This is review page.</Layout></section>;
+};
+export default Review;
