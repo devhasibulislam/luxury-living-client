fb7c5ed4f419f085090aa20a54ea3da2c6297c44
hasibulislam999 <hasib143sl@gmail.com>
2022-08-11 14:05:37 +0600

    index route modified

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import Services from "../components/Services";
 import Testimonials from "../components/Testimonials";
 import { Toaster } from "react-hot-toast";
 import Head from "next/head";
+import Header from "../shared/Header";
+import Footer from "../shared/Footer";
 
 const Index = () => {
   return (
     <section>
       <Head>
-        <title>Luxury Living</title>
+        <title>Home - Luxury Living</title>
         <meta property="og:image" content="/luxury_living.png" />
         <meta property="og:type" content="website" />
         <meta
@@ -20,12 +22,14 @@ const Index = () => {
         <meta property="og:title" content="Luxury Living" />
         <meta property="og:description" content="Reinvent your luxury" />
       </Head>
+      <Header />
       <Banner />
       <Projects />
       <Services />
       <Testimonials />
       <Contact />
       <Toaster />
+      <Footer />
     </section>
   );
 };
