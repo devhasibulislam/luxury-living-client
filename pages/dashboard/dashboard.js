0f3bcc1dc1a14a488c1ab734410b400e689ebd07
hasibulislam999 <hasib143sl@gmail.com>
2022-08-11 14:05:16 +0600

    dashboard route modified

---
+++ pages/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Sidebar from "./sidebar";
+const Dashboard = () => {
+  return (
+    <section>
+      <Sidebar />
+      This is dashboard.
+    </section>
+  );
+};
+export default Dashboard;
