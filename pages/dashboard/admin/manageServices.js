5c832380436102b451a06f0fcb1553fed9a2bc26
hasibulislam999 <hasib143sl@gmail.com>
2022-08-11 14:04:33 +0600

    manage services route modified

---
+++ pages/dashboard/admin/manageServices.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Layout from "../../../components/Layout";
+const ManageServices = () => {
+  return <section><Layout>This is manage services page.</Layout></section>;
+};
+export default ManageServices;
