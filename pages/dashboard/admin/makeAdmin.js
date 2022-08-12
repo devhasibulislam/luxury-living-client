742b94369509e60735b3726a93d71255eb0950f4
hasibulislam999 <hasib143sl@gmail.com>
2022-08-12 09:10:19 +0600

    make admin modified

--- pages/dashboard/admin/makeAdmin.js
+++ pages/dashboard/admin/makeAdmin.js
@@ -11,27 +11,29 @@ const MakeAdmin = () => {
         <title>Make Admin - Luxury Living</title>
       </Head>
       <Layout>
-        <Mockup>
-          <section>
-            <form>
-              <div className="grid grid-cols-1 gap-y-4 lg:min-w-[500px] md:min-w-[450px]">
-                <div className="flex flex-col gap-y-4">
-                  <h2 className="text-2xl font-bold">Email Address</h2>
-                  <input
-                    className={styles.inputter}
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Enter Email Address"
-                  />
+        <section className="lg:w-1/2 md:w-3/4 w-full">
+          <Mockup>
+            <section>
+              <form>
+                <div className="grid grid-cols-1 gap-y-4 lg:min-w-[500px] md:min-w-[450px]">
+                  <div className="flex flex-col gap-y-4">
+                    <h2 className="text-2xl font-bold">Email Address</h2>
+                    <input
+                      className={styles.inputter}
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Enter Email Address"
+                    />
+                  </div>
                 </div>
-              </div>
-              <button className="btn btn-md rounded bg-[#251D58] text-white w-fit px-8 capitalize mt-4">
-                Submit
-              </button>
-            </form>
-          </section>
-        </Mockup>
+                <button className="btn btn-md rounded bg-[#251D58] text-white w-fit px-8 capitalize mt-4">
+                  Submit
+                </button>
+              </form>
+            </section>
+          </Mockup>
+        </section>
       </Layout>
     </section>
   );
