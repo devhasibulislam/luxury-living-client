80424040ebd11cd183f04994e4ce242b4438fb23
hasibulislam999 <hasib143sl@gmail.com>
2022-08-12 09:10:05 +0600

    layout modified

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,12 @@ import Sidebar from "../pages/dashboard/sidebar";
 const Layout = ({ children }) => {
   return (
     <section>
-      <div className="fixed h-screen lg:px-2 md:px-1 flex justify-center items-center bg-[#eef4f8] z-50 shadow">
+      <div className="fixed top-0 right-0 h-screen lg:px-2 md:px-1 flex justify-center items-center bg-[#eef4f8] z-50 shadow">
         <Sidebar />
       </div>
       {/* <div className="bg-[#f4f7fc] flex-1 p-4 lg:ml-[3.4rem] md:ml-12 ml-10 mr-4">{children}</div> */}
-      <div className="bg-[#f4f7fc] h-screen w-screen flex justify-center items-center">{children}</div>
+      {/* <div className="bg-[#f4f7fc] h-screen w-screen flex justify-center items-center">{children}</div> */}
+      <div className="m-4">{children}</div>
     </section>
   );
 };
