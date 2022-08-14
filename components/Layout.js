6342f7f4ef09479d19ec1ec43611aec070c47b91
hasibulislam999 <hasib143sl@gmail.com>
2022-08-15 01:24:03 +0600

    layout modified

--- components/Layout.js
+++ components/Layout.js
@@ -6,8 +6,6 @@ const Layout = ({ children }) => {
       <section className="fixed top-0 right-0 h-screen lg:px-2 md:px-1 flex justify-center items-center bg-[#eef4f8] z-50 shadow">
         <Sidebar />
       </section>
-      {/* <div className="bg-[#f4f7fc] flex-1 p-4 lg:ml-[3.4rem] md:ml-12 ml-10 mr-4">{children}</div> */}
-      {/* <div className="bg-[#f4f7fc] h-screen w-screen flex justify-center items-center">{children}</div> */}
       <section className="container mx-auto">
         <div className="lg:mt-12 md:m-12 m-4">{children}</div>
       </section>
