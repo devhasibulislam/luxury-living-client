202ab770d1cffb92ff79d9746d7a5c6aa81234df
hasibulislam999 <hasib143sl@gmail.com>
2022-08-15 19:50:27 +0600

    make admin updated

--- pages/dashboard/admin/makeAdmin.js
+++ pages/dashboard/admin/makeAdmin.js
@@ -1,10 +1,39 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../../components/Layout";
 import Mockup from "../../../components/Mockup";
 import styles from "../../../styles/customer/book.module.css";
+import useSWR from "swr";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const MakeAdmin = () => {
+  const [visible, setVisible] = useState(false);
+  const [email, setEmail] = useState(null);
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data: users } = useSWR("http://localhost:5000/user", fetcher, {
+    revalidateOnFocus: true,
+  });
+
+  if (!users) return <p>Loading...</p>;
+
+  const handleAdmin = async (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+
+    const { data } = await axios.put(`http://localhost:5000/user/${email}`, {
+      email: email,
+    });
+
+    if (data?.acknowledged) {
+      toast.success(`${email} treated as Admin.`);
+      setEmail(null);
+      event.target.reset();
+    }
+  };
+
   return (
     <section>
       <Head>
@@ -14,7 +43,7 @@ const MakeAdmin = () => {
         <section className="w-full">
           <Mockup>
             <section>
-              <form>
+              <form onSubmit={handleAdmin}>
                 <div className="grid grid-cols-1 gap-y-4 lg:min-w-[500px] md:min-w-[450px]">
                   <div className="flex flex-col gap-y-4">
                     <h2 className="text-2xl font-bold">Email Address</h2>
@@ -23,8 +52,25 @@ const MakeAdmin = () => {
                       type="email"
                       name="email"
                       id="email"
+                      value={email}
                       placeholder="Enter Email Address"
+                      autoComplete="off"
+                      onChange={(event) => setEmail(event.target.value)}
+                      onClick={() => setVisible(true)}
                     />
+                    {visible && (
+                      <div className="flex flex-col gap-y-2 h-[88px] z-10 overflow-auto">
+                        {users.map((user) => (
+                          <p
+                            key={user._id}
+                            className="bg-white p-2 rounded w-full"
+                            onClick={() => setEmail(user.email)}
+                          >
+                            {user.email}
+                          </p>
+                        ))}
+                      </div>
+                    )}
                   </div>
                 </div>
                 <button className="btn btn-md rounded bg-[#251D58] text-white w-fit px-8 capitalize mt-4">
@@ -33,6 +79,10 @@ const MakeAdmin = () => {
               </form>
             </section>
           </Mockup>
+          <span
+            className="fixed w-screen h-screen bg-white top-0 left-0 -z-10"
+            onClick={() => setVisible(false)}
+          ></span>
         </section>
       </Layout>
     </section>
