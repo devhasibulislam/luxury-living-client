f097e3cd47d9ed47ea50af197a8b3c3e2f7f5b4f
hasibulislam999 <hasib143sl@gmail.com>
2022-08-15 19:50:42 +0600

    order list updated

--- pages/dashboard/admin/orderList.js
+++ pages/dashboard/admin/orderList.js
@@ -1,7 +1,29 @@
+import axios from "axios";
 import Head from "next/head";
 import React from "react";
+import useSWR from "swr";
 import Mockup from "../../../components/Mockup";
+import toast from "react-hot-toast";
+
 const OrderList = () => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data: booking } = useSWR(
+    "http://localhost:5000/customer/booking",
+    fetcher,
+    { revalidateOnFocus: true }
+  );
+
+  const handleStatus = async (id, status) => {
+    const { data } = await axios.put("http://localhost:5000/customer/booking", {
+      id,
+      status,
+    });
+    
+    if(data?.acknowledged){
+      toast.success("Status updated!");
+    }
+  };
+
   return (
     <section>
       <Head>
@@ -22,91 +44,36 @@ const OrderList = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr className="hover">
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>ganderton433@outlook.com</td>
-                  <td>Office Interior Design</td>
-                  <td>Paypal</td>
-                  <td>
-                    <select className="select select-sm max-w-xs shadow w-[105px]">
-                      <option disabled selected>
-                        Done
-                      </option>
-                      <option className="text-red-700">Pending</option>
-                      <option className="text-orange-700">Ongoing</option>
-                      <option className="text-green-700">Done</option>
-                    </select>
-                  </td>
-                </tr>
-                <tr className="hover">
-                  <th>2</th>
-                  <td>Cy Ganderton</td>
-                  <td>ganderton433@outlook.com</td>
-                  <td>Office Interior Design</td>
-                  <td>Creadit Card</td>
-                  <td>
-                    <select className="select select-sm max-w-xs shadow w-[105px]">
-                      <option disabled selected>
-                        Ongoing
-                      </option>
-                      <option className="text-red-700">Pending</option>
-                      <option className="text-orange-700">Ongoing</option>
-                      <option className="text-green-700">Done</option>
-                    </select>
-                  </td>
-                </tr>
-                <tr className="hover">
-                  <th>3</th>
-                  <td>Cy Ganderton</td>
-                  <td>ganderton433@outlook.com</td>
-                  <td>Office Interior Design</td>
-                  <td>Creadit Card</td>
-                  <td>
-                    <select className="select select-sm max-w-xs shadow w-[105px]">
-                      <option disabled selected>
-                        Pending
-                      </option>
-                      <option className="text-red-700">Pending</option>
-                      <option className="text-orange-700">Ongoing</option>
-                      <option className="text-green-700">Done</option>
-                    </select>
-                  </td>
-                </tr>
-                <tr className="hover">
-                  <th>4</th>
-                  <td>Cy Ganderton</td>
-                  <td>ganderton433@outlook.com</td>
-                  <td>Office Interior Design</td>
-                  <td>Paypal</td>
-                  <td>
-                    <select className="select select-sm max-w-xs shadow w-[105px]">
-                      <option disabled selected>
-                        Pending
-                      </option>
-                      <option className="text-red-700">Pending</option>
-                      <option className="text-orange-700">Ongoing</option>
-                      <option className="text-green-700">Done</option>
-                    </select>
-                  </td>
-                </tr>
-                <tr className="hover">
-                  <th>5</th>
-                  <td>Cy Ganderton</td>
-                  <td>ganderton433@outlook.com</td>
-                  <td>Office Interior Design</td>
-                  <td>Creadit Card</td>
-                  <td>
-                    <select className="select select-sm max-w-xs shadow w-[105px]">
-                      <option disabled selected>
-                        Ongoing
-                      </option>
-                      <option className="text-red-700">Pending</option>
-                      <option className="text-orange-700">Ongoing</option>
-                      <option className="text-green-700">Done</option>
-                    </select>
-                  </td>
-                </tr>
+                {booking?.map((book) => (
+                  <tr className="hover" key={book._id}>
+                    <th>1</th>
+                    <td>{book?.name}</td>
+                    <td>{book?.email}</td>
+                    <td>{book?.service}</td>
+                    <td>{book?.payment_method}</td>
+                    <td>
+                      <select
+                        className="select select-sm max-w-xs shadow w-[105px]"
+                        onChange={(event) =>
+                          handleStatus(book._id, event.target.value)
+                        }
+                      >
+                        <option disabled selected>
+                          {book?.status}
+                        </option>
+                        <option value="Pending" className="text-red-700">
+                          Pending
+                        </option>
+                        <option value="Ongoing" className="text-orange-700">
+                          Ongoing
+                        </option>
+                        <option value="Done" className="text-green-700">
+                          Done
+                        </option>
+                      </select>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
