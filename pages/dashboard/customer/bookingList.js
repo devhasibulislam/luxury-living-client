2bcafa9cff25c32daace5c8fc6949dbaca744ed3
hasibulislam999 <hasib143sl@gmail.com>
2022-08-15 19:50:57 +0600

    book list updated

--- pages/dashboard/customer/bookingList.js
+++ pages/dashboard/customer/bookingList.js
@@ -3,17 +3,15 @@ import React, { useEffect, useState } from "react";
 import Layout from "../../../components/Layout";
 import Mockup from "../../../components/Mockup";
 import Head from "next/head";
+import useSWR from "swr";
 
 const BookingList = () => {
-  const [services, setServices] = useState([]);
-  useEffect(() => {
-    const getServces = async () => {
-      const request = await fetch("/api/services");
-      const response = await request.json();
-      setServices(response);
-    };
-    getServces();
-  }, []);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data: services } = useSWR(
+    "http://localhost:5000/customer/booking",
+    fetcher,
+    { revalidateOnFocus: true }
+  );
 
   return (
     <section>
@@ -24,28 +22,28 @@ const BookingList = () => {
         <section className="w-full">
           <Mockup>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-              {services.map((service) => (
+              {services?.map((service) => (
                 <div
                   key={service._id}
                   className="bg-white p-4 rounded-lg shadow flex flex-col gap-y-2"
                 >
                   <div className="grid grid-cols-2">
-                    <span>
+                    {/* <span>
                       <Image
                         src={service.thumbnail}
                         alt={service.title}
                         height={60}
                         width={60}
                       />
-                    </span>
+                    </span> */}
                     <span className="text-right">
                       <span
                         className={`px-4 py-2 rounded ${
-                          (service.status === "Done" &&
+                          (service?.status === "Done" &&
                             "bg-green-300 text-green-800") ||
-                          (service.status === "Pending" &&
+                          (service?.status === "Pending" &&
                             "bg-red-300 text-red-800") ||
-                          (service.status === "Ongoing" &&
+                          (service?.status === "Ongoing" &&
                             "bg-orange-300 text-orange-700")
                         }`}
                       >
@@ -53,9 +51,10 @@ const BookingList = () => {
                       </span>
                     </span>
                   </div>
-                  <h3 className="font-bold text-xl">{service.title}</h3>
+                  <h3 className="font-bold text-xl">{service.name}</h3>
                   <p className="text-justify text-gray-500">
-                    {service.description}
+                    {service.service}
+                    {/* {service.description} */}
                   </p>
                 </div>
               ))}
