ee545e973eecc316df40d8f2b19242e78c722155
hasibulislam999 <hasib143sl@gmail.com>
2022-08-17 09:21:23 +0600

    preloader added

--- pages/dashboard/customer/bookingList.js
+++ pages/dashboard/customer/bookingList.js
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "../../../components/Layout";
 import Mockup from "../../../components/Mockup";
 import Head from "next/head";
@@ -12,6 +11,8 @@ const BookingList = () => {
     fetcher,
     { revalidateOnFocus: true }
   );
+
+  if (!services) return <p>Loading...</p>;
 
   return (
     <section>
@@ -28,17 +29,19 @@ const BookingList = () => {
                   className="bg-white p-4 rounded-lg shadow flex flex-col gap-y-2"
                 >
                   <div className="grid grid-cols-2">
-                    {/* <span>
-                      <Image
-                        src={service.thumbnail}
-                        alt={service.title}
-                        height={60}
-                        width={60}
-                      />
-                    </span> */}
+                    <span>
+                      <picture>
+                        <img
+                          src={`https://ll-luxury-living.herokuapp.com/${service.avatar}`}
+                          alt={service.name}
+                          className="w-[70px] h-[70px] object-cover rounded-full shadow-xl"
+                          style={{ maxWidth: "100%" }}
+                        />
+                      </picture>
+                    </span>
                     <span className="text-right">
                       <span
-                        className={`px-4 py-2 rounded ${
+                        className={`px-4 py-2 rounded block w-fit ml-auto ${
                           (service?.status === "Done" &&
                             "bg-green-300 text-green-800") ||
                           (service?.status === "Pending" &&
@@ -54,7 +57,7 @@ const BookingList = () => {
                   <h3 className="font-bold text-xl">{service.name}</h3>
                   <p className="text-justify text-gray-500">
                     {service.service}
-                    {/* {service.description} */}
+                    {service.description}
                   </p>
                 </div>
               ))}
